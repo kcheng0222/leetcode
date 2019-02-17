8c144d476c7d6ca4001d459ff972629d998ab3ce
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-02-16 18:18:44 -0800

    Create swap_nodes_in_pairs.py

---
+++ swap_nodes_in_pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def swapPairs(self, head: 'ListNode') -> 'ListNode':
+        if head and head.next:
+            tmp = head.next
+            head.next = self.swapPairs(tmp.next)
+            tmp.next = head
+            return tmp
+        return head
+
+//  https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681
